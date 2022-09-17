41550e810d7ecae28a81e19c71af7c6f0e706e1a
Juri Jung <36740502+mobssie@users.noreply.github.com>
2022-09-17 22:53:09 +0900

    Create 소수만들기.js

---
+++ 소수만들기.js
@@ -0,0 +1,25 @@
+/**
+ * @return {Array} [2,3,4, ... 99, 100]
+ */
+function getNums () {
+    return arr = Array.from({length: 99},((_, i)=> i+2))
+}
+
+/**
+ * @param nums {Array} [2,3,4, ... 99, 100]
+ */
+function getPrimeNumbers (nums) {
+    let answer = [];
+    let dec =[2, 3, 5];
+    answer = nums.filter(item=> item % 2 !== 0)
+      .filter(item=> item % 3 !== 0)
+      .filter(item=> item % 5 !== 0)
+    return [...dec, ...answer]
+}
+
+console.log(
+    getPrimeNumbers(
+        getNums()
+    ),
+    // [2,3,5,7,11,13, ... 97]
+)
