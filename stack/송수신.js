5d037ead86495bb10c1725bf533862d8785a0e89
mobssie <mobssie.jung@gmail.com>
2022-10-04 20:16:08 +0900

    이진수

--- stack/송수신.js
+++ stack/송수신.js
@@ -1,40 +1,27 @@
 function solution(arr){
-  // 자료구조를 만들기 (인덱스, 높이)
-  // 1. function 
-  // 2. class 
-  // 3. { h: 1, i : 1 }
-  class Pos {
-    constructor (h, i) {
-     this.h = h
-      this.i = i
+  let stack = [];
+  let answer = [];
+
+  for(let i=arr.length-1; i >= 0; i--) {
+    stack.push(arr[i]);
+  }
+  console.log('stack', stack)
+  for(let i=0; i<stack.length; i++){
+    for(let j=i+1; j < stack.length; j++){
+      if(stack[i] <= stack[j]){
+        console.log('i :: ', i)
+        console.log('j :: ', j)
+        console.log('arr[i]', arr[i])
+        console.log('arr[j]', arr[j])
+        answer.push(arr.length-j)
+      } else {
+        console.log('i ::: ', i)
+        answer.push(0);
+      }
     }
   }
-  
-  let stack = []
-  for(let i = 0 ; i < arr.length ; i++) {
-     stack.push(
-      new Pos(arr[i], i)
-     )
-  }
-  
-  for(let x of stack){
-    
-  }
-  
-  
-  
-  // 스택에 넣어
-//   console.log('arrList', arrList)
-//   for(let i = 0; i < arrList.length; i++){
-//     if(arrList[i][0] < arrList[i+1][0]) {
-//       stack.push(arrList[i+1][1])
-//     } else {
-//       stack.push(0)
-//     }
-
-//     console.log('stack', stack)
-//   }
+  console.log('answer :: ',answer)
 }
 
-let arr = [6, 9, 5, 10, 4];
-solution(arr)
+let arrList = [6, 9, 5, 7, 4];
+solution(arrList);
